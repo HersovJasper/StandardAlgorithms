f1dadbedb217599830cc1b14b3c598da5192f1a2
Jasper Hersov <Hersov.J@etoncollege.org.uk>
2020-11-16 17:59:46 +0000

    Completed Bubble Sort

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -10,6 +10,28 @@ import Foundation
 class Sorting {
     
     func bubbleSort(data: [Int]) -> [Int] {
-        return [1,3,5,8]
+        var sortedArray = data
+        var swaps = 1
+        var index = 0
+        var temp = Int()
+        while swaps != 0{
+            swaps = 0
+            index = 0
+            while index < sortedArray.count - 1 {
+                if sortedArray[index] > sortedArray[index + 1]{
+                    temp = sortedArray[index + 1]
+                    sortedArray[index + 1] = sortedArray[index]
+                    sortedArray[index] = temp
+                    swaps += 1
+                } else{
+                    index += 1
+                }
+            }
+        }
+        return sortedArray
     }
-}
+    
+    func mergeSort(data: [Int]) -> [Int] {
+        var sortedArray = data
+        
+    }
--- StandardAlgorithmsTests/SortingTest.swift
+++ StandardAlgorithmsTests/SortingTest.swift
@@ -9,7 +9,7 @@ import XCTest
 
 class SortingTest: XCTestCase {
 
-    func testBubbleSortWithIntegerArrayReturnsSortedArray() {
+    func testSortWithIntegerArrayReturnsSortedArray() {
         //arrange
         let sorting = Sorting()
         let expected = [1,3,5,8]
@@ -19,17 +19,28 @@ class SortingTest: XCTestCase {
         XCTAssertEqual(result, expected)
     }
     
-    func testBubbleSortWithVariousIntegerArraysReturnsEachOneSorted() {
+    func testSortWithVariousIntegerArraysReturnsEachOneSorted() {
         //arrange
         let sorting = Sorting()
-        let testData = [(data: [6,3,2,9,2], expected: [1,2,3,6,9]),
+        let testData = [(data: [6,3,2,9,2], expected: [2,2,3,6,9]),
                         (data: [1,100, 4, 3, 15], expected: [1,3,4,15,100])]
         //act
         for test in testData {
             let result = sorting.bubbleSort(data: test.data)
+            //asswert
             XCTAssertEqual(result, test.expected)
         }
-        
-        //assert
     }
+    
+    func testSortWithEmptyArrayReturnsEmptyArray() {
+        //arrange
+        let sorting = Sorting()
+        let expected = [Int]()
+        //act
+        let result = sorting.bubbleSort(data: [])
+        //assert
+        XCTAssertEqual(result, expected)
+    }
+    
+    
 }
