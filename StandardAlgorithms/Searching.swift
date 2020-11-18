f458d097a5fbf8b12ecb56b53bc565a8dcb1119a
Jasper Hersov <Hersov.J@etoncollege.org.uk>
2020-11-18 22:14:56 +0000

    Completed Test-Driven Development for all 6 standard algorithms

--- StandardAlgorithms/Searching.swift
+++ StandardAlgorithms/Searching.swift
@@ -9,4 +9,33 @@ import Foundation
 
 class Searching {
     
+    //LINEAR SEARCH//
+    func linearSearch(data: [Int], value: Int) -> Bool{
+      for number in data{
+        if number == value{
+          return true
+        }
+      }
+      return false
+    }
+    
+    
+    //BINARY SEARCH//
+    func binarySearch(data: [Int], value: Int) -> Bool{
+      var lowerBound = 0
+      var upperBound = data.count - 1
+      var midpoint: Int
+      while lowerBound <= upperBound{
+        midpoint = (lowerBound + upperBound)/2
+        if data[midpoint] == value {
+          return true
+        } else if data[midpoint] < value  {
+          lowerBound = midpoint + 1
+        } else{
+          upperBound = midpoint - 1
+        }
+      }
+      return false
+    }
+    
 }
--- StandardAlgorithmsTests/SearchingTest.swift
+++ StandardAlgorithmsTests/SearchingTest.swift
@@ -9,4 +9,49 @@ import XCTest
 
 class SearchingTest: XCTestCase {
     
+    func testLinearSearchWithEmptyArrayReturnsFalse() {
+        //arrange
+        let searching = Searching()
+        let expected = false
+        //act
+        let result = searching.linearSearch(data: [], value: 7)
+        //assert
+        XCTAssertEqual(result, expected)
+    }
+    
+    func testLinearSearchWithVariousIntegerArraysReturnsBoolean() {
+        //arrange
+        let searching = Searching()
+        let testData = [(data: [6,3,2,9,2], value: 9, expected: true),
+                        (data: [1,100, -4, 3, 15], value: 17, expected: false)]
+        //act
+        for test in testData {
+            let result = searching.linearSearch(data: test.data, value: test.value)
+            //asswert
+            XCTAssertEqual(result, test.expected)
+        }
+    }
+    
+    func testBinarySearchWithEmptyArrayReturnsFalse() {
+        //arrange
+        let searching = Searching()
+        let expected = false
+        //act
+        let result = searching.binarySearch(data: [], value: 7)
+        //assert
+        XCTAssertEqual(result, expected)
+    }
+    
+    func testBinarySearchWithVariousIntegerArraysReturnsBoolean() {
+        //arrange
+        let searching = Searching()
+        let testData = [(data: [6,3,2,9,2], value: 9, expected: true),
+                        (data: [1,100, -4, 3, 15], value: 17, expected: false)]
+        //act
+        for test in testData {
+            let result = searching.binarySearch(data: test.data, value: test.value)
+            //asswert
+            XCTAssertEqual(result, test.expected)
+        }
+    }
 }
